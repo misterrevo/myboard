8d7e1ec7f3fe3a274d8d1d98c0f25b7625b29ab1
mrrevo55 <kwolny31@gmail.com>
2022-03-21 20:49:38 +0100

    Removed invalid controller tests

---
+++ src/test/java/com/revo/myboard/BaseIT.java
@@ -0,0 +1,2 @@
+package com.revo.myboard;public class BaseIT {
+}
---
+++ src/test/java/com/revo/myboard/category/CategoryControllerTest.java
@@ -0,0 +1,23 @@
+package com.revo.myboard.category;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CategoryControllerTest {
+
+    @Test
+    void getCategoryById() {
+    }
+
+    @Test
+    void createCategory() {
+    }
+
+    @Test
+    void deleteCategoryByName() {
+    }
+
+    @Test
+    void renameCategoryByName() {
+    }
+}
